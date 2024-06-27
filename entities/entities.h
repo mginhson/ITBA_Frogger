b8d8280aa7665aacae75d87dba4e054228f55b9b
mginhson <mginhson@itba.edu.ar>
2024-06-27 10:52:09 -0300

    m

--- entities/entities.h
+++ entities/entities.h
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
+#ifndef ENTITIES_H
+#define ENTITIES_H
 
-
-
-=======
-#ifndef ENTITY
-#define ENTITY
 #include "allegro/assets.h"
->>>>>>> 6894fea8702dff10a0576c67c8985ff6108a7ff1
 typedef struct {
     char is_animating;
     assets_t * animation_frame;
 } animation_t;
+
+
 #endif
