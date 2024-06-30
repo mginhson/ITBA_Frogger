61ccbc1f3874f4f90265722606d8d36723d93a94
SWainer <swainer@itba.edu.ar>
2024-06-30 17:24:02 -0300

    Mundo

--- mundo/renderWorld.h
+++ mundo/renderWorld.h
@@ -1,8 +1,8 @@
 #ifndef RENDERWORLD_H
 #define RENDERWORLD_H
 
-#include "entities.h"
-#include "config.h"
+#include "../entities/entities.h"
+#include "../config.h"
 
 void renderWorld (map_t * map, independent_object_t * frog[],int size ,int tiempo);
 extern int contador;
